896d7d22782ed3008eb62e33832c7e7babac952b
Shahrose <shahrose.khan@lyftyfy.com>
2023-10-17 00:23:01 +0500

    Completed simple react app with local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,51 @@
-import { useState } from "react";
-import './styles.css';
+import { useEffect, useState } from "react";
+import "./styles.css";
+import { NewTodoForm } from "./NewTodoForm";
+import { TodoList } from "./TodoList";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState(()=>{
+    const localValue = localStorage.getItem("ITEMS");
+    if(localValue === null) return [];
+    return JSON.parse(localValue);
+  });
+
+  useEffect(()=>{
+    localStorage.setItem("ITEMS", JSON.stringify(todos));
+  }, [todos])
+
+  function addTodo(title) {
+    setTodos((currentTodos) => {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID(), title: title, completed: false },
+      ];
+    });
+  }
+
+  function toggleTodo(id, completed) {
+    setTodos((currentTodos) => {
+      return currentTodos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed };
+        }
+        return todo;
+      });
+    });
+  }
+
+  function deleteTodo(id) {
+    setTodos((currentTodos) => {
+      return currentTodos.filter((todo) => todo.id !== id);
+    });
+  }
 
   return (
-    <form className="new-item-form">
-      <div className="form-row">
-        <label htmlFor="item">New Item</label>
-        <input type="text" id="item" />
-      </div>
-    </form>
+    <>
+      <NewTodoForm onSubmit={addTodo} />
+      <h1 className="header">Todo List</h1>
+      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
+    </>
   );
 }
 
